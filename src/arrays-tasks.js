0dca6ff457886c69c938da178a721d0645c7b812
oibioib <pavelbodal@gmail.com>
2024-12-02 18:33:58 +0100

    feat: implement getTail

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -216,8 +216,10 @@ function getHead(arr, n) {
  *    getTail([ 'a', 'b', 'c', 'd'], 3) => [ 'b', 'c', 'd' ]
  *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  const { length } = arr;
+  const sliced = arr.slice(length - n, length);
+  return sliced;
 }
 
 /**
