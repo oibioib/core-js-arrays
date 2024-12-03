973ac118bc6635e3f4b54f950ba3f3b4d7d001c5
oibioib <pavelbodal@gmail.com>
2024-12-03 18:26:01 +0100

    feat: implement createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -352,8 +352,19 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return arr.reduce((result, elem) => {
+    const { length } = result;
+    const lastChunk = result[length - 1];
+
+    if (!lastChunk || lastChunk.length === chunkSize) {
+      result.push([elem]);
+    } else {
+      lastChunk.push(elem);
+    }
+
+    return result;
+  }, []);
 }
 
 /**
